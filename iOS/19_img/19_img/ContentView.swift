e42ec7ecf36b4a59979fa0412082303ae2e37833
Goncalo Feliciano <goncaloaaf@gmail.com>
2025-01-13 12:52:11 +0000

    imagens

--- iOS/19_img/19_img/ContentView.swift
+++ iOS/19_img/19_img/ContentView.swift
@@ -10,10 +10,34 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image("img1")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 200, height: 200)
+                .padding()
+            
+          /*  AsyncImage(url: URL(string: "https://i.kinja-img.com/image/upload/c_fill,h_675,pg_1,q_80,w_1200/cee10372f4c9fde8937464cc739d762d.jpg")!)
+            */
+            
+            AsyncImage(url: URL(string: "https://i.kinja-img.com/image/upload/c_fill,h_675,pg_1,q_80,w_1200/cee10372f4c9fde8937464cc739d762d.jpg")!) { img in
+                
+                img
+                    .resizable()
+                .scaledToFill()
+                .frame(width: 200, height: 200)
+                
+                
+            } placeholder: {
+                ProgressView()
+            }
+            .padding()
+            
+            Image(systemName: "figure.walk")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .foregroundStyle(.green)
+            
+                
         }
         .padding()
     }
