017cf5f519a8e250ebc26081d3c6e7749889eadc
Goncalo Feliciano <goncaloaaf@gmail.com>
2025-01-13 11:41:10 +0000

    TextFileld binding

---
+++ iOS/17_save_data_API/17_save_data_API/MyTextField.swift
@@ -0,0 +1,18 @@
+//
+//  myTFView.swift
+//  17_save_data_API
+//
+//  Created by Gonçalo Feliciano on 13/01/2025.
+//
+
+import SwiftUI
+
+struct MyTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyTextField()
+}
